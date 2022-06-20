0f6ece34fa71042ab0abcdf25d0d113d206f5e01
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-06-20 15:38:17 +0530

    Time: 1 ms (72.97%), Space: 44.1 MB (58.78%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,23 @@
+
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode curr = head;
+    if(head == null) return null;
+	
+	// if the head.next == null it means that the list contains only one node (head) 
+	//so there is no duplicates  we will simply return the head of the list
+	
+    if(head.next == null) return head;
+    else{
+        while(curr.next != null){
+		//we compare the data of the current node with the next node if they are equal
+		//we remove the next node 
+            if(curr.val == curr.next.val) 
+                curr.next = curr.next.next;
+            else
+			//if they are not equal we simply move forward
+                curr=curr.next;
+        }
+    }
+    return head;
+}}
