60b95bbf8fb005fbbce27dcd16311625b520ae9e
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-03 15:03:09 +0530

    Time: 0 ms (100.00%), Space: 41 MB (44.01%) - LeetHub

---
+++ 343-integer-break/343-integer-break.java
@@ -0,0 +1,9 @@
+class Solution {
+   public int integerBreak(int n) {
+    if (n == 2) return 1;
+    if (n == 3) return 2;
+    if (n % 3 == 0) return (int)Math.pow(3, n / 3);
+    if (n % 3 == 1) return (int)Math.pow(3, (n - 4) / 3) * 4;// we cannot have a small number that is equal to 1. Then use 4 instead.
+    return (int)Math.pow(3, (n - 2) / 3) * 2;
+}
+}
