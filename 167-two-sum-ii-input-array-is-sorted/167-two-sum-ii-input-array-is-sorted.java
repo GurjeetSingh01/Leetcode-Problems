1c133bebf41b1d4c35f97379d57086062e72e8a1
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-10 16:27:50 +0530

    Time: 3 ms (36.24%), Space: 50.7 MB (7.98%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,10 +1,35 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target){
+// int l = 0, r = nums.length-1; 
+// while(nums[l] + nums[r] != target){
+// if(nums[l] + nums[r] < target) l++;
+// else r-- ; 
+// }
+// return new int[] {l+1, r+1};
+// }
+// }
+
 class Solution {
-    public int[] twoSum(int[] nums, int target){
-int l = 0, r = nums.length-1; 
-while(nums[l] + nums[r] != target){
-if(nums[l] + nums[r] < target) l++;
-else r-- ; 
-}
-return new int[] {l+1, r+1};
-}
+    public int[] twoSum(int[] numbers, int target) {
+        int a_pointer = 0;
+        int b_pointer = numbers.length - 1;
+        int num_a, num_b;
+        
+        while(a_pointer < b_pointer) {
+            num_a = numbers[a_pointer];
+            num_b = numbers[b_pointer];
+            
+            if(num_a + num_b == target) break;
+            
+            if(num_a + num_b < target) {
+                a_pointer++;
+                continue;
+            }
+            
+            b_pointer--;
+            
+        }
+        
+        return new int[]{a_pointer + 1, b_pointer + 1};
+    }
 }
