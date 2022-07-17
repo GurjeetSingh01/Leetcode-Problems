9a75fd8200046c90ea2bc94a76819d3fd5f83e03
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-17 16:21:37 +0530

    Time: 2 ms (34.29%), Space: 43.9 MB (17.45%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,54 +1,26 @@
-public class Solution {
-      public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> res = new ArrayList<List<Integer>>();
-//         if (root == null)
-//             return res;
-//         Queue<TreeNode> q = new LinkedList<TreeNode>();
-//         q.offer(root);
-//         while (!q.isEmpty()) {
-//             List<Integer> tmp = new ArrayList<Integer>();
-//             int size = q.size();
-//             for (int i = 0; i < size; ++i) {
-//                 TreeNode t = q.poll();
-//                 if (t.left != null)
-//                     q.offer(t.left);
-//                 if (t.right != null)
-//                     q.offer(t.right);
-//                 tmp.add(t.val);
-//             }
-//             res.add(tmp);
-//         }
-//         return res;
-//     }
-// }
-
-          
-           Queue<TreeNode> queue = new LinkedList<TreeNode>();
-    
-    List<List<Integer>> fList = new LinkedList<List<Integer>>();
-    
-    if(root == null) {
-        return fList;
-    }
-    
-    queue.offer(root);
-    
-    while(!queue.isEmpty()) {
-        int levSum = queue.size();
-        List<Integer> sublist = new LinkedList<Integer>();
-        for(int i=0; i<levSum; i++) {
-            if(queue.peek().left != null) {
-                queue.offer(queue.peek().left);
-            } if(queue.peek().right != null) {
-                queue.offer(queue.peek().right);
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        if (root == null) return res;
+        
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
             }
-            sublist.add(queue.poll().val);
+            res.add(level);
         }
-        fList.add(sublist);
+        return res;
     }
-    
-    return fList;
-}}
-
-
-
+}
