da9a3ca575997627da7f826a0b6147dd719630e4
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-22 20:43:11 +0530

    Time: 46 ms (69.74%), Space: 42.2 MB (91.84%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+	public boolean canPartition(int[] nums) {
+		int sum=0;
+		for(int i:nums) sum+=i;
+		if(sum%2!=0) return false;
+		return find(nums,sum/2);
+
+	}
+	public boolean find(int []nums,int t){
+		boolean dp[]=new boolean[t+1];
+		dp[0]=true;
+		for(int i=0;i<nums.length;i++){
+			for(int j=t;j>0;j--){
+				if(j>=nums[i]) dp[j]|= dp[j-nums[i]]; 
+			}
+		}
+		return dp[t];
+	}
+}
