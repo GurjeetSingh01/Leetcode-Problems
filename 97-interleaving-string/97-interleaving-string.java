3813164fed0c554330617523d776acf4a5475f74
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-23 18:33:49 +0530

    Time: 27 ms (6.49%), Space: 48.7 MB (6.87%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,30 +1,17 @@
-class Solution{
-public boolean isInterleave(String s1, String s2, String s3){
-int n1 = s1.length();
-int n2 = s2.length();
-if(n1+n2 != s3.length())
-return false;
-boolean dp[][] = new boolean[n1+1][n2+1];
-dp[0][0] = true;
-for(int j =1; j<=n2; j++) 
-{ // initializing 0th row
-if(s2.charAt(j-1) == s3.charAt(j-1))
-dp[0][j]= dp[0][j-1];
+public class Solution {
+    Set<String> mem = new HashSet<String>();
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length() == 0 && s2.length() == 0 && s3.length() == 0)
+            return true;
+        if(mem.contains(s1+"#"+s2)) 
+            return false;
+        if(s3.length() > 0){
+            if(s1.length() > 0 &&  s1.charAt(0) == s3.charAt(0))
+                if(isInterleave(s1.substring(1),s2,s3.substring(1))) return true;
+            if(s2.length() > 0 && s2.charAt(0) == s3.charAt(0))
+                if(isInterleave(s1,s2.substring(1),s3.substring(1))) return true;
+        }
+        mem.add(s1+"#"+s2);
+        return false;
+    }
 }
-for(int i=1; i<=n1; i++)
-{// initializing 0th column
-if(s1.charAt(i-1) == s3.charAt(i-1))
-dp[i][0] = dp[i-1][0];
-}
-for(int i =1; i<=n1; i++){
-for(int j=1; j<=n2; j++)
-{// the length of s3 in n1 + n2, since the string is 0 indexed we check at i+j-1
-if(s3.charAt(i+j-1)== s1.charAt(i-1))
-dp[i][j] = dp[i-1][j];
-if(s3.charAt(i+j-1)==s2.charAt(j-1))
-dp[i][j]= dp[i][j] || dp[i][j-1];
-//In some cases the character from s3 matches both s1 and s2, therefore we consider the value thats true
-
-}}
-return dp[n1][n2];
-}}
