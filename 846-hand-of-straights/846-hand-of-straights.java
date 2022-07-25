f40f93637f5ce207915cd27487433a3b3df6b6ae
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-25 17:32:29 +0530

    Time: 51 ms (76.39%), Space: 54.5 MB (61.84%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.java
@@ -0,0 +1,18 @@
+class Solution {
+public boolean isNStraightHand(int[] hand, int W){
+PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+for(int i: hand){
+minHeap.add(i);
+}
+while(minHeap.size() != 0){
+int strt = minHeap.poll();
+for(int j = 1; j<W; j++){
+if(minHeap.remove(strt+j)){
+continue;
+}
+else {
+return false; 
+}
+}}
+return true; 
+}}
