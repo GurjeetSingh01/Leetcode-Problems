888277f159485ad25ddacd56cbad5bfd1c26672e
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-07-28 16:21:10 +0530

    Time: 9 ms (42.81%), Space: 44.8 MB (41.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,31 @@
-public class Solution{
-public boolean isAnagram(String s, String t){
-if(s.length() != t.length()) return false; 
-char[] cs = s.toCharArray();
-char[] ct = t.toCharArray();
-int[] map = new int[127];
-int cnt =0;
-for(int i =0; i<cs.length; i++){
-if(++map[cs[i]] ==1) cnt++;
-if(--map[ct[i]] == 0) cnt--;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+//         if(s.length() == t.length())
+//             return true;
+
+//         for(int i =0; i<s.length(); i++){
+//             for(int j =0; j<t.length(); j++){
+//                 if(s.charAt(i)== t.charAt(j))
+//                     return true;
+//             }
+//         }
+//         return false; 
+        
+    
+
+ char[] charArray1 = s.toCharArray();
+      char[] charArray2 = t.toCharArray();
+
+      // sort the char array
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+      // if sorted char arrays are same
+      // then the string is anagram
+      boolean result = Arrays.equals(charArray1, charArray2);
+    return result; 
+
+        
+        
 }
-return cnt==0;
-}}
+ }
