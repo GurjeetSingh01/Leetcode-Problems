0e3cbac8062c0e95e3c045cbf9aea0102b73b988
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-08-04 11:08:51 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (45.83%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        
+        while(q % 2 == 0 && p % 2 == 0){
+            q /=2;
+            p /= 2;
+        }
+        
+        //using our observations here
+        if(q % 2 == 0 && p % 2 != 0){
+            return 0;
+        }
+         if(q % 2 != 0 && p % 2 == 0){
+            return 2;
+        }
+         if(q % 2 != 0 && p % 2 != 0){
+            return 1;
+        }
+        return -1;
+    }
+}
