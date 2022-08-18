63d92e6b6c47b4d1179bf9be6298c079fbd7ec7f
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-08-18 17:58:16 +0530

    Time: 98 ms (50.00%), Space: 87 MB (52.79%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int minSetSize(int[] arr) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i:arr){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b)-> b.getValue() - a.getValue());
-        for(Map.Entry<Integer,Integer> mapElement : hm.entrySet()){
-                        // System.out.println(mapElement.getKey()+"  "+mapElement.getValue());
+public int minSetSize(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        PriorityQueue<Integer> countValues = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int num : arr) countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        for (int value : countMap.values()) countValues.offer(value);
 
-            pq.add(mapElement);
+        int size = arr.length; int result = 0;
+        while (size > arr.length / 2) {
+            size -= countValues.poll();
+            result++;
         }
-        int n = arr.length ,set =0 ;
-        while(arr.length/2 < n) {
-            Map.Entry<Integer,Integer> curr = pq.remove();
-         // System.out.println(curr.getKey()+"  "+curr.getValue());
-                set++;
-            // System.out.println(curr.getKey());
-                n -= curr.getValue();
-            }
-        
-        return set;
+
+        return result;
     }
 }
