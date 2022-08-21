8ccee93ade182f4decf515d21ef0201e12260b6e
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-08-21 14:09:06 +0530

    Time: 2 ms (57.23%), Space: 42 MB (20.23%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,21 +1,20 @@
-
-//Approach 2: 92% Faster Dynamic Programming -  Bottom Up Approach
-// Time Complexity: O(n*target) | Space Complexity: O(target)
 class Solution {
-    static int[] dp;
     public int combinationSum4(int[] nums, int target) {
-        
-        dp = new int[target + 1];
+        if (nums == null || nums.length == 0 || target <= 0) {
+            return 0;
+        }
 
+        int[] dp = new int[target + 1];
         dp[0] = 1;
-        for(int i=1; i<=target; i++){
-            for(int n : nums){
-                if(i>=n){
-                    dp[i] += dp[i-n];
+
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) {
+                if (i >= n) {
+                    dp[i] += dp[i - n];
                 }
             }
         }
+
         return dp[target];
     }
 }
-        
