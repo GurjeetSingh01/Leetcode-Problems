69ab296f586241bc59613c42b57415296606038b
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-08-31 23:55:44 +0530

    Time: 2 ms (82.98%), Space: 54.7 MB (60.55%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,42 +1,59 @@
-public class Solution {
-public void moveZeroes(int[] nums) {
+// public class Solution {
+// public void moveZeroes(int[] nums) {
 
-    //if only 1 element is present, return
-    if(nums.length==1)
-        return;
+//     //if only 1 element is present, return
+//     if(nums.length==1)
+//         return;
 
-    int i=0;
+//     int i=0;
     
-    //iterate through the array counting number of zeros
-    for(int k:nums)
-    {
-        if(k==0)
-            i++;
-    }
+//     //iterate through the array counting number of zeros
+//     for(int k:nums)
+//     {
+//         if(k==0)
+//             i++;
+//     }
         
-    //if no 0's exists or array only contains zeros , return
-    if(i==0 || i==nums.length)
-        return;
+//     //if no 0's exists or array only contains zeros , return
+//     if(i==0 || i==nums.length)
+//         return;
     
-    //index to keep track of non-zero elements
-    int m=0;
+//     //index to keep track of non-zero elements
+//     int m=0;
     
-    //for loop to move all the non zero elements in order
-    for(int j=0;j<nums.length;j++)
-    {
-        if(nums[j]!=0)
-            nums[m++]=nums[j];
+//     //for loop to move all the non zero elements in order
+//     for(int j=0;j<nums.length;j++)
+//     {
+//         if(nums[j]!=0)
+//             nums[m++]=nums[j];
+//     }
+    
+//     //insert the 0's in remaining spaces up till the end
+//     for(int j=nums.length-i;j<nums.length;j++)
+//     {
+//         nums[j]=0;
+//     }
+    
+    
+    
+    
+    
+// }
+// }
+
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){// we will shift all the non zero values to right
+                nums[index]=nums[i];
+                index++;// keep on increasing the index as well
+            }
+        }
+        for(int i= index; i<nums.length;i++){ //now we will replace all the remaing indexes with zero
+            nums[i]=0;
+        }
     }
-    
-    //insert the 0's in remaining spaces up till the end
-    for(int j=nums.length-i;j<nums.length;j++)
-    {
-        nums[j]=0;
-    }
-    
-    
-    
-    
-    
-}
 }
