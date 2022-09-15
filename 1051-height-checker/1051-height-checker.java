223da4ab88b65a267a609ffa159bbc73071908cd
Gurjeet Singh <55250674+GurjeetSingh01@users.noreply.github.com>
2022-09-15 23:02:16 +0530

    Time: 3 ms (38.55%), Space: 42.2 MB (31.99%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int heightChecker(int[] heights) 
+    {
+        var c=0;
+        int[] expected=Arrays.copyOf(heights,heights.length);
+        Arrays.sort(expected);
+        for(int i=0;i<heights.length;i++)
+        {
+            if(heights[i]!=expected[i])
+                c++;
+        }
+        return c;
+    }
+}
